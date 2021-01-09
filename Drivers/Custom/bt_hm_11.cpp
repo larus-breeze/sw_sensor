45d6525c508e940bf73b4c2772dff8bf17f44471
Maximilian Betz <Maximilian.Betz@awi.de>
2021-01-09 19:13:18 +0100

    DMA Tx working for Bluetooth.

--- Drivers/Custom/bt_hm_11.cpp
+++ Drivers/Custom/bt_hm_11.cpp
@@ -3,19 +3,25 @@
 #include "uart6.h"
 #include "stdio.h"
 
+
+static char altitude[]  = "$PGRMZ,246,f,3*1B\r\n";
+
 void bluetooth_hm_11(void*)
 {
+	acquire_privileges();
+	UART6_Init();
+
 	// Reset HM 11. Pull low for 100ms to reset
 	HAL_GPIO_WritePin(BL_RESETB_GPIO_Port, BL_RESETB_Pin, GPIO_PIN_RESET);
 	delay(100);
 	HAL_GPIO_WritePin(BL_RESETB_GPIO_Port, BL_RESETB_Pin, GPIO_PIN_SET);
 	delay(200);
 
-	char altitude[] = "$PGRMZ,246,f,3*1B\r\n";
 	uint8_t len  = strlen(altitude);
 	for(;;)
 	{
 		UART6_Transmit((uint8_t *)altitude, len);
+
 		delay(1000);
 
 		//if(UART6_Receive(&rxData, 1) == true)
--- Drivers/Custom/uart6.cpp
+++ Drivers/Custom/uart6.cpp
@@ -1,19 +1,32 @@
 #include "uart6.h"
 #include "my_assert.h"
+#include "FreeRTOS.h"
+#include "queue.h"
+#define UART6_DEFAULT_TIMEOUT 100
 
 extern UART_HandleTypeDef huart6;
-#define UART6_DEFAULT_TIMEOUT 100
+static QueueHandle_t UART6_CPL_Message_Id = NULL;
+
 
 void UART6_Init(void)
 {
-
+	if (UART6_CPL_Message_Id == NULL)
+	{
+		UART6_CPL_Message_Id =  xQueueCreate(1,0);
+	}
 }
 
 void UART6_Transmit(uint8_t *pData, uint16_t Size)
 {
 	HAL_StatusTypeDef status = HAL_OK;
-	status = HAL_UART_Transmit(&huart6, pData, Size, UART6_DEFAULT_TIMEOUT);
+	BaseType_t queue_status = pdFALSE;
+
+	status = HAL_UART_Transmit_DMA(&huart6, pData, Size);
 	ASSERT(HAL_OK == status);
+	queue_status = xQueueReceive(UART6_CPL_Message_Id, 0, UART6_DEFAULT_TIMEOUT);
+	ASSERT(pdTRUE == queue_status);
+
+	//status = HAL_UART_Transmit(&huart6, pData, Size, UART6_DEFAULT_TIMEOUT);
 }
 
 bool UART6_Receive(uint8_t *pData, uint8_t Size)
@@ -29,3 +42,45 @@ bool UART6_Receive(uint8_t *pData, uint8_t Size)
 		return false;
 	}
 }
+
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
+{
+	BaseType_t xHigherPriorityTaskWokenByPost = pdFALSE;
+	BaseType_t queue_status;
+
+	if (huart->Instance == USART6)
+	{
+		queue_status = xQueueSendFromISR(UART6_CPL_Message_Id, 0, &xHigherPriorityTaskWokenByPost);
+	}
+	else
+	{
+		ASSERT(0);
+	}
+	ASSERT(pdTRUE == queue_status);
+	portYIELD_FROM_ISR(xHigherPriorityTaskWokenByPost);
+
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+
+}
+
+void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart)
+{
+
+}
+
+void HAL_UART_AbortCpltCallback(UART_HandleTypeDef *huart)
+{
+
+}
+
+/*
+void HAL_UART_IRQHandler(UART_HandleTypeDef *huart);
+
+void HAL_UART_TxHalfCpltCallback(UART_HandleTypeDef *huart)
+{
+
+}
+*/
