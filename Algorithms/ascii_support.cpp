b2fff8755288d5a706c5a3282eb6c2fddc058398
schaefer <schaefer@lbox>
2021-01-24 18:29:59 +0100

    Added algorithm folder. Project still inconsistent.

---
+++ Algorithms/ascii_support.cpp
@@ -0,0 +1,222 @@
+/** ***********************************************************************
+ * @file		ascii_support.c
+ * @brief		Simple and fast ASCII converters
+ * @author		Dr. Klaus Schaefer
+ **************************************************************************/
+
+#include "memory.h"
+#include "ascii_support.h"
+
+float string2float(char *input)
+{
+	float result = 0.0;
+	while( *input != '.')
+	{
+		if( (*input < '0') || (*input > '9'))
+			return result;
+		result = result * 10.0 + (*input - '0');
+		++input;
+	}
+	++input;
+	float factor = 0.1;
+	while( (*input >= '0') && (*input <= '9'))
+	{
+		result = result + factor * (*input - '0');
+		factor *= 0.1;
+		++input;
+	}
+	return result;
+}
+
+static ROM char ASCIItable[]="zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz";
+
+char* itoa(int value, char* result, int base)
+{
+	// check that the base if valid
+	if (base < 2 || base > 36) { *result = '\0'; return result; }
+
+	char* ptr = result, *ptr1 = result, tmp_char;
+	int tmp_value;
+
+	do {
+		tmp_value = value;
+		value /= base;
+		*ptr++ = ASCIItable[35 + (tmp_value - value * base)];
+	} while ( value );
+
+	// Apply negative sign
+	if (tmp_value < 0) *ptr++ = '-';
+	*ptr-- = '\0';
+	while(ptr1 < ptr) {
+		tmp_char = *ptr;
+		*ptr--= *ptr1;
+		*ptr1++ = tmp_char;
+	}
+	return result;
+}
+
+static ROM char hexdigits[] = "0123456789abcdef";
+
+void utox(uint32_t value, char* result, uint8_t nibbles)
+{
+	for( int i=0; i < nibbles; ++i)
+	{
+		*result = hexdigits[(value >> 4*(nibbles-1)) & 0x0f];
+		value <<= 4;
+		++result;
+	}
+	*result=0;
+}
+void lutox(uint32_t value, char* result)
+{
+  utox( value>>32, result, 8);
+  utox( value & 0xffffffff, result+8, 8);
+}
+
+/**************************************************
+ *
+ *    ftoa - converts float to string
+ *
+ ***************************************************
+ *
+ *    This is a simple implemetation with rigid
+ *    parameters:
+ *            - Buffer must be 8 chars long
+ *            - 3 digits precision max
+ *            - absolute range is -524,287 to 524,287
+ *            - resolution (epsilon) is 0.125 and
+ *              always rounds down
+ **************************************************/
+ char* ftoa(float Value, char* Buffer)
+ {
+     union
+     {
+         float f;
+
+         struct
+         {
+             unsigned int    mantissa_lo : 16;
+             unsigned int    mantissa_hi : 7;
+             unsigned int     exponent : 8;
+             unsigned int     sign : 1;
+         };
+     } helper;
+
+     unsigned long mantissa;
+     signed char exponent;
+     unsigned int int_part;
+     char frac_part[3];
+     int i, count = 0;
+
+     helper.f = Value;
+     //mantissa is LS 23 bits
+     mantissa = helper.mantissa_lo;
+     mantissa += ((unsigned long) helper.mantissa_hi << 16);
+     //add the 24th bit to get 1.mmmm^eeee format
+     mantissa += 0x00800000;
+     //exponent is biased by 127
+     exponent = (signed char) helper.exponent - 127;
+
+     //too big to shove into 8 chars
+     if (exponent > 18)
+     {
+         Buffer[0] = 'I';
+         Buffer[1] = 'n';
+         Buffer[2] = 'f';
+         Buffer[3] = '\0';
+         return Buffer;
+     }
+
+     //too small to resolve (resolution of 1/8)
+     if (exponent < -3)
+     {
+         Buffer[0] = '0';
+         Buffer[1] = '\0';
+         return Buffer;
+     }
+
+     count = 0;
+
+     //add negative sign (if applicable)
+     if (helper.sign)
+     {
+         Buffer[0] = '-';
+         count++;
+     }
+
+     //get the integer part
+     int_part = mantissa >> (23 - exponent);
+     //convert to string
+     itoa(int_part, &Buffer[count], 10);
+
+     //find the end of the integer
+     for (i = 0; i < 8; i++)
+         if (Buffer[i] == '\0')
+         {
+             count = i;
+             break;
+         }
+
+     //not enough room in the buffer for the frac part
+     if (count > 5)
+         return Buffer;
+
+     //add the decimal point
+     Buffer[count++] = '.';
+
+     //use switch to resolve the fractional part
+     switch (0x7 & (mantissa  >> (20 - exponent)))
+     {
+         case 0:
+             frac_part[0] = '0';
+             frac_part[1] = '0';
+             frac_part[2] = '0';
+             break;
+         case 1:
+             frac_part[0] = '1';
+             frac_part[1] = '2';
+             frac_part[2] = '5';
+             break;
+         case 2:
+             frac_part[0] = '2';
+             frac_part[1] = '5';
+             frac_part[2] = '0';
+             break;
+         case 3:
+             frac_part[0] = '3';
+             frac_part[1] = '7';
+             frac_part[2] = '5';
+             break;
+         case 4:
+             frac_part[0] = '5';
+             frac_part[1] = '0';
+             frac_part[2] = '0';
+             break;
+         case 5:
+             frac_part[0] = '6';
+             frac_part[1] = '2';
+             frac_part[2] = '5';
+             break;
+         case 6:
+             frac_part[0] = '7';
+             frac_part[1] = '5';
+             frac_part[2] = '0';
+             break;
+         case 7:
+             frac_part[0] = '8';
+             frac_part[1] = '7';
+             frac_part[2] = '5';
+             break;
+     }
+
+     //add the fractional part to the output string
+     for (i = 0; i < 3; i++)
+         if (count < 7)
+             Buffer[count++] = frac_part[i];
+
+     //make sure the output is terminated
+     Buffer[count] = '\0';
+
+     return Buffer;
+ }
+
---
+++ Algorithms/quaternion.h
@@ -0,0 +1,217 @@
+/***********************************************************************//**
+ * @file		quaternion.hpp
+ * @brief		quaternion implementation
+ * @author		Dr. Klaus Schaefer
+ **************************************************************************/
+
+#ifndef QUATERNION_H
+#define QUATERNION_H
+
+#include "vector.h"
+#include "float3matrix.h"
+#include "float3vector.h"
+#include "euler.h"
+#include "asin_atan.h"
+#include "arm_math.h"
+#include "vsqrtf.h"
+
+//! quaternion - special form of vector<4>
+template <class datatype > class quaternion: public vector <datatype, 4>
+{
+public:
+	//! constructor from eulerwinkel
+	quaternion( vector <datatype, 3> &init)
+	{
+		from_euler( init[0], init[1], init[2]);
+	}
+
+	//! constructor from datatype[4]
+	quaternion( )
+	: vector <datatype, 4>({0})
+	  {
+	    vector <datatype, 4>::e[0]=1;
+	  };
+
+	//! normalize quaternion absolute value to 1.0
+	inline void normalize(void)
+	{
+		datatype tmp = vector<datatype, 4>::e[0] * vector<datatype, 4>::e[0]
+		             + vector<datatype, 4>::e[1] * vector<datatype, 4>::e[1]
+		             + vector<datatype, 4>::e[2] * vector<datatype, 4>::e[2]
+		             + vector<datatype, 4>::e[3] * vector<datatype, 4>::e[3] ;
+#if QUATERNION_NORMALIZE_FAST == 1
+		tmp = 1.0 - ((tmp - 1.0) / 2.0);
+#else
+		tmp = 1 / tmp;
+		tmp = VSQRTF( tmp);
+#endif
+		for( int i = 0; i < 4; ++i)
+			vector<datatype, 4>::e[i] *= tmp;
+	};
+
+	//! quaternion -> euler angle transformation
+	operator eulerangle <datatype> () const
+	{
+		datatype e0 = vector<datatype, 4>::e[0];
+		datatype e1 = vector<datatype, 4>::e[1];
+		datatype e2 = vector<datatype, 4>::e[2];
+		datatype e3 = vector<datatype, 4>::e[3];
+
+		eulerangle <datatype> _euler;
+
+		//! formula from roenbaeck p34
+		_euler.r = my_atan2f(  2.0 * (e0*e1 + e2*e3) , e0*e0 - e1*e1 - e2*e2 + e3*e3 );
+		_euler.n = my_asinf(   2.0 * (e0*e2 - e3*e1));
+		_euler.y = my_atan2f(  2.0 * (e0*e3 + e1*e2) , e0*e0 + e1*e1 - e2*e2 - e3*e3 );
+		return _euler;
+	}
+	//! linearized euler component e0 (approximate nick angle / rad)
+	inline datatype lin_e0( void) const
+	{
+		datatype e0 = vector<datatype, 4>::e[0];
+		datatype e1 = vector<datatype, 4>::e[1];
+		datatype e2 = vector<datatype, 4>::e[2];
+		datatype e3 = vector<datatype, 4>::e[3];
+		return 2.0 * (e0*e1 + e2*e3) / ( e0*e0 - e1*e1 - e2*e2 + e3*e3);
+
+	}
+	//! linearized euler component e1 (approximate roll angle / rad)
+	inline datatype lin_e1( void) const
+	{
+		datatype e0 = vector<datatype, 4>::e[0];
+		datatype e1 = vector<datatype, 4>::e[1];
+		datatype e2 = vector<datatype, 4>::e[2];
+		datatype e3 = vector<datatype, 4>::e[3];
+		return 2.0f * (e0*e2 - e3*e1);
+	}
+	//! euler component e2
+	inline datatype get_e2( void) const
+	{
+		datatype e0 = vector<datatype, 4>::e[0];
+		datatype e1 = vector<datatype, 4>::e[1];
+		datatype e2 = vector<datatype, 4>::e[2];
+		datatype e3 = vector<datatype, 4>::e[3];
+		return my_atan2f(  2.0 * (e0*e3 + e1*e2) , e0*e0 + e1*e1 - e2*e2 - e3*e3 );
+	}
+
+	//! get north component of attitude
+	inline datatype get_north( void) const
+	{
+		datatype e0 = vector<datatype, 4>::e[0];
+		datatype e1 = vector<datatype, 4>::e[1];
+		datatype e2 = vector<datatype, 4>::e[2];
+		datatype e3 = vector<datatype, 4>::e[3];
+		return e0*e0 + e1*e1 - e2*e2 - e3*e3;
+	}
+
+	//! get east component of attitude
+	inline datatype get_east( void) const
+	{
+		datatype e0 = vector<datatype, 4>::e[0];
+		datatype e1 = vector<datatype, 4>::e[1];
+		datatype e2 = vector<datatype, 4>::e[2];
+		datatype e3 = vector<datatype, 4>::e[3];
+		return 2.0 * (e0*e3 + e1*e2);
+	}
+
+	//! get down component of attitude
+	inline datatype get_down( void) const
+	{
+		datatype e0 = this->e[0];
+		datatype e1 = this->e[1];
+		datatype e2 = this->e[2];
+		datatype e3 = this->e[3];
+		return 2.0 * (e1*e3-e0*e2);
+	}
+
+	//! quaternion update using rotation vector
+	void rotate( datatype p, datatype q, datatype r)
+	{
+		datatype e0 = vector<datatype, 4>::e[0];
+		datatype e1 = vector<datatype, 4>::e[1];
+		datatype e2 = vector<datatype, 4>::e[2];
+		datatype e3 = vector<datatype, 4>::e[3];
+
+		//! R.Rogers formula 2.92
+		vector<datatype, 4>::e[0] += (-e1*p -e2*q -e3*r);
+		vector<datatype, 4>::e[1] += ( e0*p +e2*r -e3*q);
+		vector<datatype, 4>::e[2] += ( e0*q -e1*r +e3*p);
+		vector<datatype, 4>::e[3] += ( e0*r +e1*q -e2*p);
+
+		normalize();
+	}
+
+	//! euler angle -> quaternion transformation
+	void from_euler( datatype p, datatype q, datatype r)
+	{
+		p *= 0.5; q *= 0.5; r *= 0.5;
+		register datatype sinphi   = sinf( p);
+		register datatype cosphi   = cosf( p);
+		register datatype sintheta = sinf( q);
+		register datatype costheta = cosf( q);
+		register datatype sinpsi   = sinf( r);
+		register datatype cospsi   = cosf( r);
+
+		vector<datatype, 4>::e[0] = cosphi*costheta*cospsi + sinphi*sintheta*sinpsi;
+		vector<datatype, 4>::e[1] = sinphi*costheta*cospsi + cosphi*sintheta*sinpsi;
+		vector<datatype, 4>::e[2] = cosphi*sintheta*cospsi + sinphi*costheta*sinpsi;
+		vector<datatype, 4>::e[3] = cosphi*costheta*sinpsi - sinphi*sintheta*cospsi;
+	}
+
+	//! quaternion -> rotation matrix transformation
+	void get_rotation_matrix (matrix<datatype, 3> &m) const
+	{
+		//! R.Rogers formula 2.90
+		datatype e0=vector<datatype, 4>::e[0];
+		datatype e1=vector<datatype, 4>::e[1];
+		datatype e2=vector<datatype, 4>::e[2];
+		datatype e3=vector<datatype, 4>::e[3];
+
+	    m.e[0][0] = 2.0 * (e0*e0+e1*e1) - 1.0;
+	    m.e[0][1] = 2.0 * (e1*e2-e0*e3);
+	    m.e[0][2] = 2.0 * (e1*e3+e0*e2);
+
+	    m.e[1][0] = 2.0 * (e1*e2+e0*e3);
+	    m.e[1][1] = 2.0 * (e0*e0+e2*e2) - 1.0;
+	    m.e[1][2] = 2.0 * (e2*e3-e0*e1);
+
+	    m.e[2][0] = 2.0 * (e1*e3-e0*e2);
+	    m.e[2][1] = 2.0 * (e2*e3+e0*e1);
+	    m.e[2][2] = 2.0 * (e0*e0+e3*e3) - 1.0;
+	}
+	quaternion <datatype> operator * ( quaternion <datatype> & right) //!< quaternion multiplication
+		{
+		quaternion <datatype> result;
+		datatype e0=vector<datatype, 4>::e[0];
+		datatype e1=vector<datatype, 4>::e[1];
+		datatype e2=vector<datatype, 4>::e[2];
+		datatype e3=vector<datatype, 4>::e[3];
+		datatype re0=right.vector<datatype, 4>::e[0];
+		datatype re1=right.vector<datatype, 4>::e[1];
+		datatype re2=right.vector<datatype, 4>::e[2];
+		datatype re3=right.vector<datatype, 4>::e[3];
+		result.vector<datatype, 4>::e[0] = e0 * re0 - e1 * re1 - e2 * re2 + e3 * re3;
+		result.vector<datatype, 4>::e[1] = e1 * re0 + e2 * re3 - e3 * re2 + e0 * re1;
+		result.vector<datatype, 4>::e[2] = e3 * re1 + e0 * re2 - e1 * re3 + e2 * re0;
+		result.vector<datatype, 4>::e[3] = e1 * re2 - e2 * re1 + e0 * re3 + e3 * re0;
+		return result;
+		}
+
+	void from_rotation_matrix( matrix<datatype, 3> &rotm) //!< rotation matrix -> quaternion transformation
+		{
+		float tmp;
+		tmp = 1.0f + rotm.e[0][0] + rotm.e[1][1] + rotm.e[2][2];
+		//! formula from roenbaeck p35
+		assert(( tmp > 0.0f) && (tmp != 0.0f/0.0f /* NAN */));
+		tmp = VSQRTF( tmp);
+		tmp *= 0.5;
+		vector<datatype, 4>::e[0] = tmp;
+		tmp = 0.25f / tmp;
+		vector<datatype, 4>::e[1] = tmp * (rotm.e[2][1] - rotm.e[1][2]);
+		vector<datatype, 4>::e[2] = tmp * (rotm.e[0][2] - rotm.e[2][0]);
+		vector<datatype, 4>::e[3] = tmp * (rotm.e[1][0] - rotm.e[0][1]);
+		normalize(); // compensate computational inaccuracies
+		};
+};
+
+#endif // QUATERNION_H
