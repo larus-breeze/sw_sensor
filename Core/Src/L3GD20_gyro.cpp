042a99939bdd143e180afb86aa66e729e5e4c743
Maximilian Betz <Maximilian.Betz@awi.de>
2021-01-29 23:00:35 +0100

    added measurement values to the global database structure.

--- Core/Src/L3GD20_gyro.cpp
+++ Core/Src/L3GD20_gyro.cpp
@@ -26,7 +26,7 @@ static void runnable (void*)
     {
       L3GD20_ReadData (gyro_xyz);
       for (int i = 0; i < 3; i++)
-	 output_data.m.MEMS_gyro[i] = gyro_xyz[i] * SCALING;
+	 output_data.m.lowcost_gyro[i] = gyro_xyz[i] * SCALING;
     }
 }
 
