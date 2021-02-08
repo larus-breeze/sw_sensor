f13710e49a47ae49a234260df4ff295b9b92b681
Maximilian Betz <Maximilian.Betz@awi.de>
2021-02-08 23:05:18 +0100

    added commands but all commands seems to have no effect.

--- Drivers/Custom/bt_hm_11.cpp
+++ Drivers/Custom/bt_hm_11.cpp
@@ -42,15 +42,22 @@ RestrictedTask bluetooth_handling(bluetooth_hm_11, "BT_HM_11", 256);
 #endif
 
 
-bool Bluetooth_Cmd(uint8_t *cmd)
+void Bluetooth_SendCmd(uint8_t *cmd)
 {
-  uint8_t rxByte = 0;
-  uint8_t detector = 0;
   uint32_t length = 0;
   while(cmd[length] != 0)
     {
       length++;
     }
+  /* Send command*/
+  UART6_Transmit(cmd, length);
+}
+
+
+bool Bluetooth_Cmd(uint8_t *cmd)
+{
+  uint8_t rxByte = 0;
+  uint8_t detector = 0;
 
   /*Flush UART RX queue*/
   while(true == UART6_Receive(&rxByte))
@@ -61,7 +68,7 @@ bool Bluetooth_Cmd(uint8_t *cmd)
     }
 
   /* Send command*/
-  UART6_Transmit(cmd, length);
+  Bluetooth_SendCmd(cmd);
 
   uint32_t timeout = 1000;
   while(timeout > 0){
@@ -91,33 +98,80 @@ bool Bluetooth_Cmd(uint8_t *cmd)
   return false; /*No proper acknowledge from Bluetooth module*/
 }
 
-static uint8_t baudratecmd[] = "AT+BAUD4\r\n";
-static uint8_t baudratequerry[] = "AT+BAUD?\r\n";
-static uint8_t getpowercmd[] = "AT+POWE?\r\n";
+void Bluetooth_FlushRx(void)
+{
+  uint8_t rxByte = 0;
+  while(true == UART6_Receive(&rxByte))
+    {
+#if ITM_TRACE_ENABLE
+      ITM_SendChar(rxByte);
+#endif
+    }
+}
+
+static uint8_t baudratecmd[] = "AT+BAUD4\r\n";  /* Change to 115200 baud*/
+static uint8_t disableConnecting[] = "AT+IMME0\r\n";  /* Disable automatic connection*/
+static uint8_t enableConnecting[] = "AT+IMME1\r\n";  /* Enable automatic connection*/
+static uint8_t setName[] = "AT+NAMED1234";
+
+
+//static uint8_t baudratequerry[] = "AT+BAUD?\r\n";
+//static uint8_t getpowercmd[] = "AT+POWE?\r\n";
+//static uint8_t desirecmode[] = "AT+MODE0\r\n";   //AT- configure prior connection, transparent uart after connection
+
+void Bluetooth_Reset(void)
+{
+  // Reset HM 11. Pull low for 100ms to reset
+  HAL_GPIO_WritePin(BL_RESETB_GPIO_Port, BL_RESETB_Pin, GPIO_PIN_RESET);
+  delay(200);
+  HAL_GPIO_WritePin(BL_RESETB_GPIO_Port, BL_RESETB_Pin, GPIO_PIN_SET);
+  delay(50);
+}
+
 bool Bluetooth_Init(void)
 {
   UART6_Init();
-  // Reset HM 11. Pull low for 100ms to reset
-  HAL_GPIO_WritePin(BL_RESETB_GPIO_Port, BL_RESETB_Pin, GPIO_PIN_RESET);
-  delay(100);
-  HAL_GPIO_WritePin(BL_RESETB_GPIO_Port, BL_RESETB_Pin, GPIO_PIN_SET);
-  delay(200);
+  Bluetooth_Reset(); /* Its in reset mode prior to this.*/
 
-  //UART6_ChangeBaudRate(9600);
+   /*First disable auto connecting mode.*/
+  UART6_ChangeBaudRate(115200);
+  Bluetooth_SendCmd(disableConnecting);  /* All commands seem to not work!*/
+  UART6_ChangeBaudRate(9600);
+  Bluetooth_SendCmd(disableConnecting);
 
+  Bluetooth_SendCmd(setName);
+
+  delay(500);
+  Bluetooth_FlushRx();
+  Bluetooth_Reset();
+
+  UART6_ChangeBaudRate(9600);
+  Bluetooth_Cmd(baudratecmd);  /* Set baudrate to 115200*/
   UART6_ChangeBaudRate(115200);
 
+  /* Last step enable auto connection mode*/
+  Bluetooth_Cmd(enableConnecting);
 
-  return false;
+  /*Hope that HM-11 understood change command or already talks at 115200 baud */
+   delay(1000);  /* Give the Bluetooth module some time to process changing the Baudrate*/
+
+
+
+
+   return true;
 }
 
+
+//static char altitude[]  = "$PGRMZ,246,f,3*1B\r\n";
 void Bluetooth_Transmit(uint8_t *pData, uint16_t Size)
 {
-  UART6_Transmit(pData, Size);
+   Bluetooth_FlushRx();
+   UART6_Transmit(pData, Size);     /*TODO: seems to block startup / paring if to much data is transmitted
+					 or if not synchronized with connection status. Can we check a DIO pin? */
 }
 
 bool Bluetooth_Receive(uint8_t *pRxByte)
 {
-  UART6_Receive(pRxByte);
+  return UART6_Receive(pRxByte);
 }
 
