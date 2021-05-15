51a0db23eea454f372389c35733bcee6154ff99c
schaefer <schaefer@lbox>
2021-05-15 11:08:24 +0200

    Math beautifications.

--- Algorithms/quaternion.h
+++ Algorithms/quaternion.h
@@ -33,15 +33,15 @@ public:
 	    vector <datatype, 4>::e[0]=1;
 	  };
 
-	//! normalize quaternion absolute value to 1.0
+	//! normalize quaternion absolute value to ONE
 	inline void normalize(void)
 	{
 	  datatype tmp = vector<datatype, 4>::e[0] * vector<datatype, 4>::e[0]
 		       + vector<datatype, 4>::e[1] * vector<datatype, 4>::e[1]
 		       + vector<datatype, 4>::e[2] * vector<datatype, 4>::e[2]
 		       + vector<datatype, 4>::e[3] * vector<datatype, 4>::e[3] ;
-	  tmp = 1.0 / tmp;
-	  tmp = VSQRTF( tmp);
+	  tmp = ONE / tmp;
+	  tmp = SQRT( tmp);
 	  for( int i = 0; i < 4; ++i)
 		  vector<datatype, 4>::e[i] *= tmp;
 	};
@@ -57,9 +57,9 @@ public:
 		eulerangle <datatype> _euler;
 
 		//! formula from roenbaeck p34
-		_euler.r = my_atan2f(  2.0 * (e0*e1 + e2*e3) , e0*e0 - e1*e1 - e2*e2 + e3*e3 );
-		_euler.n = my_asinf(   2.0 * (e0*e2 - e3*e1));
-		_euler.y = my_atan2f(  2.0 * (e0*e3 + e1*e2) , e0*e0 + e1*e1 - e2*e2 - e3*e3 );
+		_euler.r = ATAN2(  TWO * (e0*e1 + e2*e3) , e0*e0 - e1*e1 - e2*e2 + e3*e3 );
+		_euler.n = ASIN(   TWO * (e0*e2 - e3*e1));
+		_euler.y = ATAN2(  TWO * (e0*e3 + e1*e2) , e0*e0 + e1*e1 - e2*e2 - e3*e3 );
 		return _euler;
 	}
 	//! linearized euler component e0 (approximate nick angle / rad)
@@ -69,7 +69,7 @@ public:
 		datatype e1 = vector<datatype, 4>::e[1];
 		datatype e2 = vector<datatype, 4>::e[2];
 		datatype e3 = vector<datatype, 4>::e[3];
-		return 2.0 * (e0*e1 + e2*e3) / ( e0*e0 - e1*e1 - e2*e2 + e3*e3);
+		return TWO * (e0*e1 + e2*e3) / ( e0*e0 - e1*e1 - e2*e2 + e3*e3);
 
 	}
 	//! linearized euler component e1 (approximate roll angle / rad)
@@ -79,7 +79,7 @@ public:
 		datatype e1 = vector<datatype, 4>::e[1];
 		datatype e2 = vector<datatype, 4>::e[2];
 		datatype e3 = vector<datatype, 4>::e[3];
-		return 2.0f * (e0*e2 - e3*e1);
+		return TWO * (e0*e2 - e3*e1);
 	}
 	//! euler component e2
 	inline datatype get_e2( void) const
@@ -88,7 +88,7 @@ public:
 		datatype e1 = vector<datatype, 4>::e[1];
 		datatype e2 = vector<datatype, 4>::e[2];
 		datatype e3 = vector<datatype, 4>::e[3];
-		return my_atan2f(  2.0 * (e0*e3 + e1*e2) , e0*e0 + e1*e1 - e2*e2 - e3*e3 );
+		return ATAN2(  TWO * (e0*e3 + e1*e2) , e0*e0 + e1*e1 - e2*e2 - e3*e3 );
 	}
 
 	//! get north component of attitude
@@ -108,7 +108,7 @@ public:
 		datatype e1 = vector<datatype, 4>::e[1];
 		datatype e2 = vector<datatype, 4>::e[2];
 		datatype e3 = vector<datatype, 4>::e[3];
-		return 2.0 * (e0*e3 + e1*e2);
+		return TWO * (e0*e3 + e1*e2);
 	}
 
 	//! get down component of attitude
@@ -118,7 +118,7 @@ public:
 		datatype e1 = this->e[1];
 		datatype e2 = this->e[2];
 		datatype e3 = this->e[3];
-		return 2.0 * (e1*e3-e0*e2);
+		return TWO * (e1*e3-e0*e2);
 	}
 
 	//! quaternion update using rotation vector
@@ -141,7 +141,7 @@ public:
 	//! euler angle -> quaternion transformation
 	void from_euler( datatype p, datatype q, datatype r)
 	{
-		p *= 0.5; q *= 0.5; r *= 0.5;
+		p *= HALF; q *= HALF; r *= HALF;
 		register datatype sinphi   = SIN( p);
 		register datatype cosphi   = COS( p);
 		register datatype sintheta = SIN( q);
@@ -164,17 +164,17 @@ public:
 		datatype e2=vector<datatype, 4>::e[2];
 		datatype e3=vector<datatype, 4>::e[3];
 
-	    m.e[0][0] = 2.0 * (e0*e0+e1*e1) - 1.0;
-	    m.e[0][1] = 2.0 * (e1*e2-e0*e3);
-	    m.e[0][2] = 2.0 * (e1*e3+e0*e2);
+	    m.e[0][0] = TWO * (e0*e0+e1*e1) - ONE;
+	    m.e[0][1] = TWO * (e1*e2-e0*e3);
+	    m.e[0][2] = TWO * (e1*e3+e0*e2);
 
-	    m.e[1][0] = 2.0 * (e1*e2+e0*e3);
-	    m.e[1][1] = 2.0 * (e0*e0+e2*e2) - 1.0;
-	    m.e[1][2] = 2.0 * (e2*e3-e0*e1);
+	    m.e[1][0] = TWO * (e1*e2+e0*e3);
+	    m.e[1][1] = TWO * (e0*e0+e2*e2) - ONE;
+	    m.e[1][2] = TWO * (e2*e3-e0*e1);
 
-	    m.e[2][0] = 2.0 * (e1*e3-e0*e2);
-	    m.e[2][1] = 2.0 * (e2*e3+e0*e1);
-	    m.e[2][2] = 2.0 * (e0*e0+e3*e3) - 1.0;
+	    m.e[2][0] = TWO * (e1*e3-e0*e2);
+	    m.e[2][1] = TWO * (e2*e3+e0*e1);
+	    m.e[2][2] = TWO * (e0*e0+e3*e3) - ONE;
 	}
 	quaternion <datatype> operator * ( quaternion <datatype> & right) //!< quaternion multiplication
 		{
@@ -197,13 +197,13 @@ public:
 	void from_rotation_matrix( matrix<datatype, 3> &rotm) //!< rotation matrix -> quaternion transformation
 		{
 		float tmp;
-		tmp = 1.0f + rotm.e[0][0] + rotm.e[1][1] + rotm.e[2][2];
+		tmp = ONE + rotm.e[0][0] + rotm.e[1][1] + rotm.e[2][2];
 		//! formula from roenbaeck p35
 		assert(( tmp > 0.0f) && (tmp != 0.0f/0.0f /* NAN */));
 		tmp = SQRT( tmp);
-		tmp *= 0.5;
+		tmp *= HALF;
 		vector<datatype, 4>::e[0] = tmp;
-		tmp = 0.25f / tmp;
+		tmp = QUARTER / tmp;
 		vector<datatype, 4>::e[1] = tmp * (rotm.e[2][1] - rotm.e[1][2]);
 		vector<datatype, 4>::e[2] = tmp * (rotm.e[0][2] - rotm.e[2][0]);
 		vector<datatype, 4>::e[3] = tmp * (rotm.e[1][0] - rotm.e[0][1]);
--- Core/Inc/embedded_math.h
+++ Core/Inc/embedded_math.h
@@ -13,6 +13,11 @@
 
 #define M_PI_F 3.14159265358979323846f
 #define M_PI   3.14159265358979323846
+#define NAN_F 0x40000000
+#define ONE 1.0f
+#define TWO 2.0f
+#define HALF 0.5f
+#define QUARTER 0.25f
 
 #define SQR(x) ((x)*(x))
 #define SQRT(x) VSQRTF(x)
@@ -21,6 +26,5 @@
 #define ASIN(x) my_asinf(x)
 #define ATAN2(y, x) my_atan2f(y, x)
 
-#define NAN_F 0x40000000
 
 #endif /* INC_EMBEDDED_MATH_H_ */
