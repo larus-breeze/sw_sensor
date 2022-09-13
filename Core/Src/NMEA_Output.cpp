83cfa66d0bda9cfb7b8d78a84139ee3e595efaeb
realtimepeople <klaus.schaefer@h-da.de>
2022-09-13 12:03:04 +0200

    NMEA output simplified. Untested.

--- Core/Src/NMEA_Output.cpp
+++ Core/Src/NMEA_Output.cpp
@@ -1,7 +1,7 @@
 #include "system_configuration.h"
 #include "FreeRTOS_wrapper.h"
 #include "common.h"
-#include "NMEA_Output.h"
+#include "NMEA_format.h"
 
 #include <bt_hm.h>
 #include "NMEA_format.h"
@@ -12,11 +12,9 @@
 
 COMMON string_buffer_t NMEA_buf;
 extern USBD_HandleTypeDef hUsbDeviceFS; // from usb_device.c
-extern float declination;
 
 static void runnable (void* data)
 {
-  float declination = *(float *)data;
 
   #if ACTIVATE_USB_NMEA
   MX_USB_DEVICE_Init();
@@ -32,39 +30,8 @@ static void runnable (void* data)
 
   for (synchronous_timer t (NMEA_REPORTING_PERIOD); true; t.sync ())
     {
-      char *next;
 
-      format_RMC (GNSS.coordinates, NMEA_buf.string);
-      next = NMEA_append_tail (NMEA_buf.string);
-
-      format_GGA (GNSS.coordinates, next);  //TODO: ensure that this reports the altitude in meter above medium sea level and height above wgs84: http://aprs.gids.nl/nmea/#gga
-      next = NMEA_append_tail (next);
-
-      format_MWV (output_data.wind_average[NORTH], output_data.wind_average[EAST], next);
-      next = NMEA_append_tail (next);
-
-#if USE_PTAS
-
-      format_PTAS1 (output_data.vario,
-		    output_data.integrator_vario,
-		    output_data.c.position.e[DOWN] * -1.0,   //TODO: PTAS shall report pure barometric altitude, based on static_pressure. As there can be a QNH applied to in XCSOAR.
-		    output_data.TAS,
-		    next);
-      next = NMEA_append_tail (next);
-#endif
-      format_POV( output_data.TAS, output_data.m.static_pressure,
-			 output_data.m.pitot_pressure, output_data.m.supply_voltage, output_data.vario, next);
-
-      if( output_data.m.outside_air_humidity > 0.0f) // report AIR data if available
-	append_POV( output_data.m.outside_air_humidity*100.0f, output_data.m.outside_air_temperature, next);
-
-      next = NMEA_append_tail (next);
-
-      append_HCHDM( output_data.euler.y - declination, next); // report magnetic heading
-
-      next = NMEA_append_tail (next);
-
-      NMEA_buf.length = next - NMEA_buf.string;
+      format_NMEA_string( output_data, NMEA_buf, 0); // todo fixme declination set to 0 for a while
 
 #if ACTIVATE_USB_NMEA
       USBD_CDC_SetTxBuffer(&hUsbDeviceFS, (uint8_t *)NMEA_buf.string, NMEA_buf.length);
@@ -81,5 +48,5 @@ static void runnable (void* data)
 
 ROM float declination_DUMMY = 2.0f; // todo fixme !
 
-COMMON RestrictedTask NMEA_task( runnable, "NMEA", 256, (void *)&declination_DUMMY, NMEA_USB_PRIORITY | portPRIVILEGE_BIT);
+COMMON RestrictedTask NMEA_task( runnable, "NMEA", 256, 0, NMEA_USB_PRIORITY | portPRIVILEGE_BIT);
 
