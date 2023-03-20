c08b137b2683a4afae55c6da01d9594144466c25
realtimepeople <klaus.schaefer@h-da.de>
2023-03-20 14:21:47 +0100

    Added default parameters for magnetic auto-calibration.

--- Communication/EEPROM_defaults.cpp
+++ Communication/EEPROM_defaults.cpp
@@ -8,60 +8,46 @@ void write_EEPROM_defaults( void)
 {
   unsigned status;
   status = EEPROM_initialize();
-  ASSERT( ! status);
+  assert( status == false);
 
   status = lock_EEPROM( false);
-  ASSERT( status == 0);
+  assert( status == false);
 
-  status = write_EEPROM_value( SENS_TILT_ROLL, 0.0f);
-  ASSERT( ! status);
-  status = write_EEPROM_value( SENS_TILT_NICK, 0.0f);
-  ASSERT( ! status);
-  status = write_EEPROM_value( SENS_TILT_YAW,  0.0f);
-  ASSERT( ! status);
+  status = 	    write_EEPROM_value( SENS_TILT_ROLL, 0.0f);
+  status = status | write_EEPROM_value( SENS_TILT_NICK, 0.0f);
+  status = status | write_EEPROM_value( SENS_TILT_YAW,  0.0f);
 
-  status = write_EEPROM_value( ANT_BASELENGTH, 1.0f);
-  ASSERT( ! status);
-  status = write_EEPROM_value( ANT_SLAVE_DOWN, 0.0f);
-  ASSERT( ! status);
-  status = write_EEPROM_value( ANT_SLAVE_RIGHT, 0.0f);
-  ASSERT( ! status);
+  status = status | write_EEPROM_value( ANT_BASELENGTH, 1.0f);
+  status = status | write_EEPROM_value( ANT_SLAVE_DOWN, 0.0f);
+  status = status | write_EEPROM_value( ANT_SLAVE_RIGHT, 0.0f);
+  status = status | write_EEPROM_value( GNSS_CONFIGURATION, (float)GNSS_M9N);
 
-  status = write_EEPROM_value( GNSS_CONFIGURATION, (float)GNSS_M9N);
-  ASSERT( ! status);
+  status = status | write_EEPROM_value( PITOT_OFFSET, 0.0f);
+  status = status | write_EEPROM_value( PITOT_SPAN, 1.0f);
+  status = status | write_EEPROM_value( QNH_OFFSET, 0.0f);
 
-  status = write_EEPROM_value( PITOT_OFFSET, 0.0f);
-  ASSERT( ! status);
-  status = write_EEPROM_value( PITOT_SPAN, 1.0f);
-  ASSERT( ! status);
-
-  status = write_EEPROM_value( QNH_OFFSET, 0.0f);
-  ASSERT( ! status);
-
-  write_EEPROM_value( MAG_X_OFF, 0.0f);
-  write_EEPROM_value( MAG_X_SCALE, 1.0f);
-  write_EEPROM_value( MAG_Y_OFF, 0.0f);
-  write_EEPROM_value( MAG_Y_SCALE, 1.0f);
-  write_EEPROM_value( MAG_Z_OFF, 0.0f);
-  write_EEPROM_value( MAG_Z_SCALE, 1.0f);
-  write_EEPROM_value( MAG_STD_DEVIATION, 0.009999f);
+  status = status | write_EEPROM_value( MAG_X_OFF, 0.0f);
+  status = status | write_EEPROM_value( MAG_X_SCALE, 1.0f);
+  status = status | write_EEPROM_value( MAG_Y_OFF, 0.0f);
+  status = status | write_EEPROM_value( MAG_Y_SCALE, 1.0f);
+  status = status | write_EEPROM_value( MAG_Z_OFF, 0.0f);
+  status = status | write_EEPROM_value( MAG_Z_SCALE, 1.0f);
+  status = status | write_EEPROM_value( MAG_STD_DEVIATION, 0.009999f);
+  status = status | write_EEPROM_value( MAG_AUTO_CALIB, 1.0f);
+  status = status | write_EEPROM_value( MAG_EARTH_AUTO, 0.0f);
 
   // time constants
-  status = write_EEPROM_value( VARIO_TC, DEFAULT_VARIO_TC);
-  ASSERT( ! status);
-  status = write_EEPROM_value( VARIO_INT_TC, DEFAULT_AVG_VARIO_TC);
-  ASSERT( ! status);
-  status = write_EEPROM_value( WIND_TC, DEFAULT_WIND_TC);
-  ASSERT( ! status);
-  status = write_EEPROM_value( MEAN_WIND_TC, DEFAULT_WIND_AVG_TC);
-  ASSERT( ! status);
-  status = write_EEPROM_value( VETF, DEFAULT_VETF);
-  ASSERT( ! status);
+  status = status | write_EEPROM_value( VARIO_TC, DEFAULT_VARIO_TC);
+  status = status | write_EEPROM_value( VARIO_INT_TC, DEFAULT_AVG_VARIO_TC);
+  status = status | write_EEPROM_value( WIND_TC, DEFAULT_WIND_TC);
+  status = status | write_EEPROM_value( MEAN_WIND_TC, DEFAULT_WIND_AVG_TC);
+  status = status | write_EEPROM_value( VETF, DEFAULT_VETF);
 
-  status = write_EEPROM_value( DECLINATION, 0.0f * M_PI_F / 180.0);
-  ASSERT( ! status);
-  status = write_EEPROM_value( INCLINATION, +65.5f * M_PI_F / 180.0);
-  ASSERT( ! status);
+  status = status | write_EEPROM_value( DECLINATION, 0.0f * M_PI_F / 180.0);
+  status = status | write_EEPROM_value( INCLINATION, +65.5f * M_PI_F / 180.0);
 
-  lock_EEPROM( true);
+  assert( status == false); // be sure there was no error
+
+  status = lock_EEPROM( true);
+  assert( status == false);
 }
