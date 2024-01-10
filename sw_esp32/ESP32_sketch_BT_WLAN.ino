fb30a8b3e9d5b60ee638e8ca6827690489ca2c09
Blaubart <Blaubart@gmx.de>
2024-01-10 21:34:51 +0100

    WiFi and Bluetooth libraries and settings are generally compiled
    WiFi and Bluetooth libraries and settings are generally compiled. This makes sense if, for example: A switch connected to the ESP should decide on the WiFi or Bluetooth mode. Then the ESP32 does not have to be reprogrammed to change the mode.

--- sw_esp32/ESP32_sketch_BT_WLAN.ino
+++ sw_esp32/ESP32_sketch_BT_WLAN.ino
@@ -12,14 +12,14 @@
 
 // start config: ////////////////////////////////////////////////////////////
 //
-//#define BLUETOOTH       // remove // ​​before #define BLUETOOTH if OpenSoar, XCSoar should connect to the Larus via Bluetooth, comment out #define WiFiMODE
-#define WiFiMODE          // remove // ​​before #define WiFiMODE if OpenSoar, XCSoar should connect to the Larus via Wifi, comment out #define BLUETOOTH
+bool WiFiMode = false;              // set WiFiMode = true and BTMode = false if OpenSoar, XCSoar should connect to the Larus via WiFi or set WiFiMode = false
+bool BTMode = true;               // and BTMode = true if OpenSoar, XCSoar should connect to the Larus via Bluetooth
 //
 // For AP mode:
 const char *ssid = "Larus";        // You will connect your OpenSoar, XCSoar to this WiFi or BT Access Point
 const char *pw = "LarusSensor";    // and this is the WiFi password
 //
-IPAddress ip(192, 168, 4, 1); 
+IPAddress ip(192, 168, 4, 1);
 IPAddress netmask(255, 255, 255, 0);
 //
 // You must connect OpenSoar, XCSoar to this AP, then:
@@ -50,29 +50,25 @@ IPAddress netmask(255, 255, 255, 0);
 #define SERIAL2_TCP_PORT 8880       // Wifi Port UART2
 
 #define bufferSize 1024
+#define MAX_NMEA_CLIENTS 4
 
 // end config.h
 #include <esp_wifi.h>
 #include <WiFi.h>
-
-#ifdef BLUETOOTH
+#include <WiFiClient.h>
 #include <BluetoothSerial.h>
+
 BluetoothSerial SerialBT;
-#endif
 
 HardwareSerial Serial_one(1);
 HardwareSerial Serial_two(2);
 HardwareSerial* COM[NUM_COM] = {&Serial, &Serial_one , &Serial_two};
 
-#define MAX_NMEA_CLIENTS 4
-#ifdef WiFiMODE
-#include <WiFiClient.h>
 WiFiServer server_0(SERIAL0_TCP_PORT);
 WiFiServer server_1(SERIAL1_TCP_PORT);
 WiFiServer server_2(SERIAL2_TCP_PORT);
 WiFiServer *server[NUM_COM] = {&server_0, &server_1, &server_2};
 WiFiClient TCPClient[NUM_COM][MAX_NMEA_CLIENTS];
-#endif
 
 uint8_t buf1[NUM_COM][bufferSize];
 uint16_t i1[NUM_COM] = {0, 0, 0};
@@ -91,30 +87,33 @@ void setup() {
   COM[1]->begin(UART_BAUD1, SERIAL_PARAM1, SERIAL1_RXPIN, SERIAL1_TXPIN);
   COM[2]->begin(UART_BAUD2, SERIAL_PARAM2, SERIAL2_RXPIN, SERIAL2_TXPIN);
 
-#ifdef WiFiMODE
-  //AP mode (phone connects directly to ESP) (no router)
-  WiFi.mode(WIFI_AP);
+  if ((BTMode == false) && (WiFiMode == true))
+  {
+    SerialBT.end();
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(ssid, pw); // configure ssid and password for softAP
+    delay(2000); // VERY IMPORTANT
+    WiFi.softAPConfig(ip, ip, netmask); // configure ip address for softAP
+  }
 
-  WiFi.softAP(ssid, pw); // configure ssid and password for softAP
-  delay(2000); // VERY IMPORTANT
-  WiFi.softAPConfig(ip, ip, netmask); // configure ip address for softAP
-#endif
+  if ((BTMode == true) && (WiFiMode == false))
+  {
+    WiFi.mode(WIFI_OFF);
+    SerialBT.begin(ssid); //Bluetooth device name
+  }
 
-#ifdef BLUETOOTH
-  SerialBT.begin(ssid); //Bluetooth device name
-#endif  
-
-#ifdef WiFiMODE
-  COM[0]->println("Starting TCP Server 1");
-  server[0]->begin(); // start TCP server
-  server[0]->setNoDelay(true);
-  COM[1]->println("Starting TCP Server 2");
-  server[1]->begin(); // start TCP server
-  server[1]->setNoDelay(true);
-  COM[2]->println("Starting TCP Server 3");
-  server[2]->begin(); // start TCP server
-  server[2]->setNoDelay(true);
-#endif
+  if ((BTMode == false) && (WiFiMode == true))
+  {
+    COM[0]->println("Starting TCP Server 1");
+    server[0]->begin(); // start TCP server
+    server[0]->setNoDelay(true);
+    COM[1]->println("Starting TCP Server 2");
+    server[1]->begin(); // start TCP server
+    server[1]->setNoDelay(true);
+    COM[2]->println("Starting TCP Server 3");
+    server[2]->begin(); // start TCP server
+    server[2]->setNoDelay(true);
+  }
 
   esp_err_t esp_wifi_set_max_tx_power(50);  //lower WiFi Power
 }
@@ -122,62 +121,64 @@ void setup() {
 void loop()
 {
 
-#ifdef BLUETOOTH
-  // receive from Bluetooth:
-  if (SerialBT.hasClient())
+  if ((BTMode == true) && (WiFiMode == false))
   {
-    while (SerialBT.available())
+    // receive from Bluetooth:
+    if (SerialBT.hasClient())
     {
-      BTbuf[iBT] = SerialBT.read(); // read char from client (LK8000 app)
-      if (iBT < bufferSize - 1) iBT++;
-    }
-    for (int num = 0; num < NUM_COM ; num++)
-      COM[num]->write(BTbuf, iBT); // now send to UART(num):
-    iBT = 0;
-  }
-#endif
-
-#ifdef WiFiMODE
-  for (int num = 0; num < NUM_COM ; num++)
-  {
-    if (server[num]->hasClient())
-    {
-      for (byte i = 0; i < MAX_NMEA_CLIENTS; i++) {
-        //find free/disconnected spot
-        if (!TCPClient[num][i] || !TCPClient[num][i].connected()) {
-          if (TCPClient[num][i]) TCPClient[num][i].stop();
-          TCPClient[num][i] = server[num]->available();
-          continue;
-        }
+      while (SerialBT.available())
+      {
+        BTbuf[iBT] = SerialBT.read(); // read char from client (LK8000 app)
+        if (iBT < bufferSize - 1) iBT++;
       }
-      //no free/disconnected spot so reject
-      WiFiClient TmpserverClient = server[num]->available();
-      TmpserverClient.stop();
+      for (int num = 0; num < NUM_COM ; num++)
+        COM[num]->write(BTbuf, iBT); // now send to UART(num):
+      iBT = 0;
     }
   }
-#endif
+  if ((BTMode == false) && (WiFiMode == true))
+  {
+    for (int num = 0; num < NUM_COM ; num++)
+    {
+      if (server[num]->hasClient())
+      {
+        for (byte i = 0; i < MAX_NMEA_CLIENTS; i++) {
+          //find free/disconnected spot
+          if (!TCPClient[num][i] || !TCPClient[num][i].connected()) {
+            if (TCPClient[num][i]) TCPClient[num][i].stop();
+            TCPClient[num][i] = server[num]->available();
+            continue;
+          }
+        }
+        //no free/disconnected spot so reject
+        WiFiClient TmpserverClient = server[num]->available();
+        TmpserverClient.stop();
+      }
+    }
+  }
 
   for (int num = 0; num < NUM_COM ; num++)
   {
     if (COM[num] != NULL)
     {
-      
-#ifdef WiFiMODE
-      for (byte cln = 0; cln < MAX_NMEA_CLIENTS; cln++)
-      {
-        if (TCPClient[num][cln])
-        {
-          while (TCPClient[num][cln].available())
-          {
-            buf1[num][i1[num]] = TCPClient[num][cln].read(); // read char from client (LK8000 app)
-            if (i1[num] < bufferSize - 1) i1[num]++;
-          }
 
-          COM[num]->write(buf1[num], i1[num]); // now send to UART(num):
-          i1[num] = 0;
+      if ((BTMode == false) && (WiFiMode == true))
+      {
+        for (byte cln = 0; cln < MAX_NMEA_CLIENTS; cln++)
+        {
+          if (TCPClient[num][cln])
+          {
+            while (TCPClient[num][cln].available())
+            {
+              buf1[num][i1[num]] = TCPClient[num][cln].read(); // read char from client (LK8000 app)
+              if (i1[num] < bufferSize - 1) i1[num]++;
+            }
+
+            COM[num]->write(buf1[num], i1[num]); // now send to UART(num):
+            i1[num] = 0;
+          }
         }
       }
-#endif
 
       if (COM[num]->available())
       {
@@ -186,23 +187,25 @@ void loop()
           buf2[num][i2[num]] = COM[num]->read(); // read char from UART(num)
           if (i2[num] < bufferSize - 1) i2[num]++;
         }
-        
-#ifdef WiFiMODE
-        // now send to WiFi:
-        for (byte cln = 0; cln < MAX_NMEA_CLIENTS; cln++)
-        {
-          if (TCPClient[num][cln])
-            TCPClient[num][cln].write(buf2[num], i2[num]);
-        }
-#endif
 
-#ifdef BLUETOOTH
-        // now send to Bluetooth:
-        if (SerialBT.hasClient())
+        if ((BTMode == false) && (WiFiMode == true))
         {
-          SerialBT.write(buf2[num], i2[num]);
+          // now send to WiFi:
+          for (byte cln = 0; cln < MAX_NMEA_CLIENTS; cln++)
+          {
+            if (TCPClient[num][cln])
+              TCPClient[num][cln].write(buf2[num], i2[num]);
+          }
         }
-#endif
+
+        if ((BTMode == true) && (WiFiMode == false))
+        {
+          // now send to Bluetooth:
+          if (SerialBT.hasClient())
+          {
+            SerialBT.write(buf2[num], i2[num]);
+          }
+        }
 
         COM[1]->write(buf2[num], i2[num]); // echo to RS232 output
         i2[num] = 0;
