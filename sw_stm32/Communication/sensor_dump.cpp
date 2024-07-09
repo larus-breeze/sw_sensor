c44b273a76f307e153b9b831d6325fd217ac51d0
realtimepeople <klaus.schaefer@h-da.de>
2024-07-09 14:10:16 +0200

    Tiny beautifications in sonsor output.

--- sw_stm32/Communication/sensor_dump.cpp
+++ sw_stm32/Communication/sensor_dump.cpp
@@ -142,7 +142,7 @@ void format_sensor_dump( const output_data_t &output_data, string_buffer_t &NMEA
   s=append_string( s, "Sats: ");
   s = format_2_digits( s, (float32_t)(output_data.c.SATS_number));
 
-  s=append_string( s, "Speed-Accuracy: ");
+  s=append_string( s, " Speed-Accuracy: ");
   s = to_ascii_2_decimals( 100.0f * (float32_t)(output_data.c.speed_acc), s);
 
   s=append_string( s, " GNSS time: ");
@@ -160,7 +160,7 @@ void format_sensor_dump( const output_data_t &output_data, string_buffer_t &NMEA
       *s++=' ';
     }
 
-  s=append_string( s, " Strength: ");
+  s=append_string( s, " Strength = ");
   s = to_ascii_2_decimals( 100.0f * output_data.nav_induction_gnss.abs(), s);
   newline( s);
 
@@ -173,8 +173,9 @@ void format_sensor_dump( const output_data_t &output_data, string_buffer_t &NMEA
   s=append_string( s, " Inclination = ");
   s = to_ascii_2_decimals( RAD_2_DEGREES_10 * inclination_decimator.get_output(), s);
 
-  s=append_string( s, " MagAnomaly / % = ");
+  s=append_string( s, " MagAnomaly = ");
   s = to_ascii_2_decimals( output_data.magnetic_disturbance * 10000.0f, s);
+  *s++ = '%';
   newline( s);
 
   if( output_data.c.sat_fix_type == (SAT_FIX | SAT_HEADING))
